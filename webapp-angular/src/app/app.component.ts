96e662b36ad4b5bb62332cbb8b230bcc0cca14aa
rsauermann <yes>
2024-05-15 21:06:17 +0200

    chore: Add debug-controls component and related files

--- webapp-angular/src/app/app.component.ts
+++ webapp-angular/src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { GraphComponent } from "./graph/graph.component";
+import { DebugControlsComponent } from "./debug-controls/debug-controls.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, GraphComponent]
+    imports: [RouterOutlet, GraphComponent, DebugControlsComponent]
 })
 
 export class AppComponent {
