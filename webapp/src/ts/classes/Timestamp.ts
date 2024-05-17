04aa3d9d48942a882c2ab458d5c6ee91563d332a
Felix Breitweiser <felix.breitweiser@uni-siegen.de>
2024-05-17 08:54:07 +0200

    removed old webapp

--- webapp/src/ts/classes/Timestamp.ts
+++
@@ -1,9 +0,0 @@
-import {JsonProperty, Serializable} from "ts-jackson";
-
-@Serializable()
-export default class Timestamp {
-    @JsonProperty()
-    logical!: number;
-    @JsonProperty()
-    physical!: Date;
-}
