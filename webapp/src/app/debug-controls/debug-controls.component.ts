443a83850ce53e87ea285b0e029deec2017977d6
rsauermann <yes>
2024-06-23 16:36:38 +0200

    chore: Update debug-controls component with input field for step time and refactor resume method

--- webapp/src/app/debug-controls/debug-controls.component.ts
+++ webapp/src/app/debug-controls/debug-controls.component.ts
@@ -1,21 +1,37 @@
 import { Component } from '@angular/core';
 import { CoreSocketFactory } from '../models/communication/CoreSocketFactory';
 import { JsonRpcWebSocketClient } from '../models/communication/RpcSocket';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-debug-controls',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './debug-controls.component.html',
   styleUrl: './debug-controls.component.css'
 })
 export class DebugControlsComponent {
   CoreSocket: JsonRpcWebSocketClient;
+  Break: boolean = false;
+  StepTime: number = 1000;
   constructor() {
     this.CoreSocket = CoreSocketFactory.create();
   }
 
   public send(data: string) {
+    this.Break = true;
     this.CoreSocket.call(data);
+  }
+
+  public async resume() {
+    this.Break = false;
+    while (!this.Break) {
+      this.CoreSocket.call('step');
+      await new Promise(resolve => setTimeout(resolve, this.StepTime));
+    }
+  }
+
+  public break() {
+    this.Break = true;
   }
 }
