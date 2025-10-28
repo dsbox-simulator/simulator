550d00408f028b438f26d1124646cee4863bcf7c
Felix Breitweiser <felix.breitweiser@uni-siegen.de>
2025-10-28 10:58:56 +0100

    disable terminal window for native processes in windows

--- dsbox-core/src/process/native.rs
+++ dsbox-core/src/process/native.rs
@@ -6,14 +6,26 @@ use tokio::sync::mpsc::{Sender, UnboundedReceiver};
 use tokio::sync::oneshot;
 use tokio::task::JoinHandle;
 
-use crate::process::{ProcessCommand, ProcessEvent};
 use crate::process::io_helper::process_io_helper;
+use crate::process::{ProcessCommand, ProcessEvent};
+
+/// An extension for [`Command`] that disables the creation of a terminal window for child
+/// processes under windows. Implemented below, does nothing outside of windows
+trait CommandExt {
+    fn disable_terminal_window(&mut self) -> &mut Self;
+}
 
 /// launches a new native process with the given `path` to an executable and the sender and receiver for commands/events
-pub(super) fn launch(path: &Path, args: &[String], command_receiver: UnboundedReceiver<ProcessCommand>, event_sender: Sender<ProcessEvent>) -> tokio::io::Result<(JoinHandle<()>, oneshot::Receiver<()>)> {
+pub(super) fn launch(
+    path: &Path,
+    args: &[String],
+    command_receiver: UnboundedReceiver<ProcessCommand>,
+    event_sender: Sender<ProcessEvent>,
+) -> tokio::io::Result<(JoinHandle<()>, oneshot::Receiver<()>)> {
     log::trace!("launching process `{}`, args: {args:?}", path.display());
     let mut child = Command::new(path)
         .args(args)
+        .disable_terminal_window()
         .stdin(Stdio::piped())
         .stdout(Stdio::piped())
         .stderr(Stdio::piped())
@@ -24,12 +36,37 @@ pub(super) fn launch(path: &Path, args: &[String], command_receiver: UnboundedRe
     let stderr = child.stderr.take().unwrap();
 
     let (finished_tx, finished_rx) = oneshot::channel();
-    Ok((tokio::task::spawn(async move {
-        process_io_helper(event_sender, command_receiver, stdin, stdout, stderr, async move {
-            match child.wait().await {
-                Ok(status) => status.code().unwrap_or(0),
-                Err(_) => -1,
-            }
-        }, finished_tx).await
-    }), finished_rx))
+    Ok((
+        tokio::task::spawn(async move {
+            process_io_helper(
+                event_sender,
+                command_receiver,
+                stdin,
+                stdout,
+                stderr,
+                async move {
+                    match child.wait().await {
+                        Ok(status) => status.code().unwrap_or(0),
+                        Err(_) => -1,
+                    }
+                },
+                finished_tx,
+            )
+            .await
+        }),
+        finished_rx,
+    ))
+}
+
+impl CommandExt for Command {
+    #[cfg(target_os = "windows")]
+    fn disable_terminal_window(&mut self) -> &mut Self {
+        const CREATE_NO_WINDOW: u32 = 0x08000000;
+        self.creation_flags(CREATE_NO_WINDOW)
+    }
+
+    #[cfg(not(target_os = "windows"))]
+    fn disable_terminal_window(&mut self) -> &mut Self {
+        self
+    }
 }
